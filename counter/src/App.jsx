9e2b250337a4c4646b76a6df26e21cab6a5a8d8b
Rajat <rajat.jas@gmail.com>
2025-10-24 11:52:57 +0530

    updated code for interview problem

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -5,7 +5,10 @@ function App() {
   // let counter = 5
   const [counter, setCounter] = useState(0)
   const increaseValue = () =>{
-    setCounter(counter + 1)
+    setCounter(counter + 1) // even though we have added the multiple counter increment. it wont work as updates are sent to Ui in batches and React fiber takes care of it
+    setCounter((prevCounter) =>{counter +1}) //best way to do if we want to update 
+    setCounter((prevCounter) =>{counter +1})
+    setCounter((prevCounter) =>{counter +1})
   }
   const decreaseValue = () => {
     setCounter(counter - 1)
